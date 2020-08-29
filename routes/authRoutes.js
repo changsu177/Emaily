c9303ddddfefc91cf084fec850bfd751dc64874b
changsu177 <59569997+changsu177@users.noreply.github.com>
2020-08-28 22:54:18 -0400

    finished auth flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,14 +9,15 @@ module.exports = app => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => {
+
+    //logout is automatically attached to req by passport
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
-
-//logout is automatically attached to req by passport
-app.get('/api/logout',(req,res) =>{
-  req.logout(),
-  res.send(req.user)
-});
-
-app.get('/api/current_user', (req, res) =>{
-  res.send(req.user)
-});
