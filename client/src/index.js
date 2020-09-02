2c9e3f79e51baf905d745f387cacd1802d83b6cb
su su <susu@sus-MacBook-Pro.local>
2020-09-02 19:50:54 -0400

    initial commit

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,12 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 
 import App from './components/App';
+//reducer,  initial state of the application,
 const store = createStore(() => [], {}, applyMiddleware());
 
 ReactDOM.render(
-  <Provider store={store}><App /></Provider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
 );
